26baf0521446a53524d4dc343b2d6cbe0ec4b5e9
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-15 22:27:59 +0700

    Delete default.php

--- default.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Expires: 0');
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
-header('Location: https://fuckablelovers.com/?utm_source=akqcGWaTnyYIo&utm_campaign=DEFAULT&utm_content=54227', true, 301);
-exit();
-?>
