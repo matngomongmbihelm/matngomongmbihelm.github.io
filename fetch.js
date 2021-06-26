17568e8c0c237c61ca7b8a7b56030e19cf36e13f
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-26 23:07:51 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,20 +1,5 @@
 $(function() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyA7aOALLRGxJcFdbAaC3zyGFowwXw0hfRM",
-        authDomain: "trackinguserhtc.firebaseapp.com",
-        databaseURL: "https://trackinguserhtc-default-rtdb.firebaseio.com",
-        projectId: "trackinguserhtc",
-        storageBucket: "trackinguserhtc.appspot.com",
-        messagingSenderId: "402421887585",
-        appId: "1:402421887585:web:d449277383fcc075a17020"
-    };
-    firebase.initializeApp(firebaseConfig);
-    var reading = firebase.database().ref('user_id/').orderByKey();
-    reading.once('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            var sick = childSnapshot.val();
-            console.log(sick);
-            $('#fetching').bootstrapTable({data : sick});
-        });
+    $.get('https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json', function(doooo){
+        $('#fetching').bootstrapTable({data : doooo});
     });
 });
