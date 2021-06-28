505b4ceefbead1637093012664317955baa4c55d
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-28 23:56:53 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -4,10 +4,10 @@ $(function() {
             var pengguna = Object.values(data).map(object => object.v.user_htc, os = Object.values(data).map(object => object.v.device_id, key_secret = Object.values(data).map(object => object.v.secret_token, data_tanggal = Object.values(data).map(object => object.v.tanggal;
             var thread_id = [
                     {
-                        'user_htc': v[x].user_htc,
-                        'device_id': v[x].device_id,
-                        'secret_token': v[x].secret_token,
-                        'tanggal': v[x].tanggal
+                        'user_htc': pengguna,
+                        'device_id': os,
+                        'secret_token': key_secret,
+                        'tanggal': data_tanggal
                      }
                ]
             $('#fetching').bootstrapTable({data: thread_id});
