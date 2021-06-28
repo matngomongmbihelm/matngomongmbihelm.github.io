a3f729d6b3eb2a3dfc349e3d25fec8c8c1818bc5
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-28 23:56:12 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,17 +1,16 @@
 $(function() {
     $.get('https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json?print=pretty', function(data){
         $.each(data, function(i, v){
-            for(var x in v){
-                var thread = [
+            var pengguna = Object.values(data).map(object => object.v.user_htc, os = Object.values(data).map(object => object.v.device_id, key_secret = Object.values(data).map(object => object.v.secret_token, data_tanggal = Object.values(data).map(object => object.v.tanggal;
+            var thread_id = [
                     {
                         'user_htc': v[x].user_htc,
                         'device_id': v[x].device_id,
                         'secret_token': v[x].secret_token,
                         'tanggal': v[x].tanggal
                      }
-                ]
-            }
-            $('#fetching').bootstrapTable({data: thread});
+               ]
+            $('#fetching').bootstrapTable({data: thread_id});
         });
     });
 });
