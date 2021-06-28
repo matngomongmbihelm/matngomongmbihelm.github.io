660e35169e62e3a478f8e191098e1286da3aab59
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-29 04:26:46 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,12 +1,15 @@
 $(function() {
     $.get('https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json?print=pretty', function(result){
-        var user = Object.values(result).map(object => object.user_htc), os = Object.values(result).map(object => object.device_id), tok = Object.values(result).map(object => object.secret_token), tang = Object.values(result).map(object => object.tanggal), show = '';
-        show += '<tr>';
-        show += '<td>'+ user +'</td>';
-        show += '<td>'+ os +'</td>';
-        show += '<td>'+ tok +'</td>';
-        show += '<td>'+ tang +'</td>';
-        show += '</tr>';
-        $('#fetching').append(show);
+        var show = '';
+        $.each(result, function(i, v){
+            var user = Object.values(v).map(object => object.user_htc), os = Object.values(v).map(object => object.device_id), tok = Object.values(v).map(object => object.secret_token), tang = Object.values(v).map(object => object.tanggal);
+            show += '<tr>';
+            show += '<td>'+ user +'</td>';
+            show += '<td>'+ os +'</td>';
+            show += '<td>'+ tok +'</td>';
+            show += '<td>'+ tang +'</td>';
+            show += '</tr>';
+            $('#fetching').append(show);
+        });
     });
 });
