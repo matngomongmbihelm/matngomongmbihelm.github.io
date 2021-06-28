f77107fbe85c25c7cebbf76a38d2a50e715f1ee1
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-29 04:19:37 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,30 +1,12 @@
 $(function() {
-    $("#fecthing").dataTable({
-        ajax : {
-            url : 'https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json?print=pretty',
-            dataSrc : function(json) {
-                var temp, item, data = [];
-                for (var i=0;i<json.length;i++) {
-                    temp = Object.values(json);
-                    item = {};
-                    for (var elem in temp) {        
-                        item[temp[elem].user_htc];
-                        item[temp[elem].device_id];
-                        item[temp[elem].secret_token];
-                        item[temp[elem].tanggal];
-                    }
-                    data.push(item);
-                }
-                return data
-            }
-        },
-        paging: true,
-        searching: true,
-        columns : [
-            { data : 'user_htc', title : 'user_htc' },
-            { data : 'device_id', title : 'device_id'  },
-            { data : 'secret_token', title : 'secret_token' },        
-            { data : 'tanggal', title : 'tanggal' }
-        ]    
+    $.get('https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json?print=pretty', function(result){
+        var user = Object.values(result).map(object => object.user_htc), os = Object.values(result).map(object => object.device_id), tok = Object.values(result).map(object => object.secret_token), tang = Object.values(result).map(object => object.tanggal), show = '';
+        show += '<tr>';
+        show += '<td>'+ user +'</td>';
+        show += '<td>'+ os +'</td>';
+        show += '<td>'+ tok +'</td>';
+        show += '<td>'+ tang +'</td>';
+        show += '</tr>';
+        $('#fetching').append(show);
     });
 });
