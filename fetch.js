8860b47e5a39ba05f83e6614aef62681be6fcbba
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-06-30 01:22:14 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,12 +1,14 @@
 $(function() {
     $.get('https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json?print=pretty', function(result){
-        var id = {}, user = Object.values(result).map(object => object.user_htc), os = Object.values(result).map(object => object.device_id), tok = Object.values(result).map(object => object.secret_token), tang = Object.values(result).map(object => object.tanggal);
-        id.push({
-            pengguna : user,
-            device : os,
-            token : tok,
-            tanggal : tang
+        var id = [], user = Object.values(result).map(object => object.user_htc), os = Object.values(result).map(object => object.device_id), tok = Object.values(result).map(object => object.secret_token), tang = Object.values(result).map(object => object.tanggal);
+        $.each(result, function(i, v){
+            id.push({
+                pengguna : user,
+                device : os,
+                token : tok,
+                tanggal : tang
+            });
+            console.log(id);
         });
-        console.log(id);
     });
 });
