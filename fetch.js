19116c7314b85909a781e97b90897ea7e5ff8951
matngomongmbihelm <85574129+matngomongmbihelm@users.noreply.github.com>
2021-07-11 23:33:14 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 $(function() {
-    $.get('https://trackinguserhtc-default-rtdb.firebaseio.com/user_id.json?print=pretty', function(result){
+    $.get('https://htcuser-22503-default-rtdb.firebaseio.com/HUNTERCOMMUNITY.json?print=pretty', function(result){
         function getSampleObject(users){
           return $.map(users, function(item, i){
               return {
